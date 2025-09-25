aa84f097be499c6ce439172597c4631654c55818
parker-sherrill <godspeedapplications@gmail.com>
2025-09-25 00:57:36 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  eslint: {
+    // 🚀 Ignore lint errors during `next build` (good for quick deploys)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
